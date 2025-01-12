0903a33069859ccd3f80cb11bc5878b29f3557b6
Hitesh Rupani <rupanihiteshabc@gmail.com>
2025-01-12 21:00:47 +0530

    added home view model

--- TheCryptoApp/TheCryptoAppApp.swift
+++ TheCryptoApp/TheCryptoAppApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct TheCryptoAppApp: App {
+    
+    @StateObject var vm = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 HomeView()
                     .toolbar(.hidden) // hiding navigation bar
             }
+            .environmentObject(vm)
         }
     }
 }
